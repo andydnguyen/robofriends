93d1ae49392d0cfb3c320634a53dbc30ccd414b6
Andy Nguyen <anguyen1119@gmail.com>
2019-11-17 10:18:49 -0500

    Add filter for robot name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
 
-class App extends React.Component {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots,
+      searchfield: ''
+    };
+  }
+  onSearchChange = e => {
+    this.setState({ searchfield: e.target.value });
+  };
   render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />;
       </div>
     );
   }
