3e2708c394072e841cbae7c12a398728f0111951
Andy Nguyen <anguyen1119@gmail.com>
2019-11-17 11:42:10 -0500

    Add floating search and use user api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots,
+      robots: [],
       searchfield: ''
     };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState({ robots: users }));
   }
   onSearchChange = e => {
     this.setState({ searchfield: e.target.value });
@@ -21,13 +26,19 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className='tc'>
-        <h1 className='f2'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />;
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f2'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />;
+          </Scroll>
+        </div>
+      );
+    }
   }
 }
 
