e7f593b38849d3bc070cf7d7f9ba4eca8babd692
Andy Nguyen <anguyen1119@gmail.com>
2019-11-17 17:08:24 -0500

    Updates

---
+++ precache-manifest.bc63a9ef680194bb649f6834e4ad97ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e3f5b85d71c01cf5e0c78414d35d65",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1d355fa0d945dc8fb51e",
+    "url": "/robofriends/static/css/main.28ce81ca.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "1d355fa0d945dc8fb51e",
+    "url": "/robofriends/static/js/main.7220b5e6.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
